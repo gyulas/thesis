fd35c07bad15e77f40502e9b3f8b4560a967dcde
Gyulai László <gyulailacko@gmail.com>
2018-11-05 19:34:04 +0100

    minor chg!

--- model/househeat_data_szakdoga.m
+++ model/househeat_data_szakdoga.m
@@ -3,57 +3,71 @@
 %   house thermodynamics example. Note that time is given in units of hours
 
 %   Copyright 1990-2012 The MathWorks, Inc.
-% -------------------------------
-% Problem constant
-% -------------------------------
-% converst radians to degrees
-r2d = 180/pi;
-% -------------------------------
-% Define the house geometry
-% -------------------------------
-% House length = 30 m
-lenHouse = 30;
-% House width = 10 m
-widHouse = 10;
-% House height = 4 m
-htHouse = 4;
-% Roof pitch = 40 deg
-pitRoof = 40/r2d;
-% Number of windows = 6
-numWindows = 6;
-% Height of windows = 1 m
-htWindows = 1;
-% Width of windows = 1 m
-widWindows = 1;
-windowArea = numWindows*htWindows*widWindows;
-wallArea = 2*lenHouse*htHouse + 2*widHouse*htHouse + ...
-           2*(1/cos(pitRoof/2))*widHouse*lenHouse + ...
-           tan(pitRoof)*widHouse - windowArea;
-% -------------------------------
-% Define the type of insulation used
-% -------------------------------
-% Glass wool in the walls, 0.2 m thick
-% k is in units of J/sec/m/C                                                       NOT convert to J/hr/m/C multiplying by 3600
-kWall = 0.038;   % second is the time unit
-LWall = .2;
-RWall = LWall/(kWall*wallArea);
-% Glass windows, 0.01 m thick
-kWindow = 0.78;  % second is the time unit
-LWindow = .01;
-RWindow = LWindow/(kWindow*windowArea);
-% -------------------------------
-% Determine the equivalent thermal resistance for the whole building
-% -------------------------------
-Req = RWall*RWindow/(RWall + RWindow);
+
+T_s=60;                     %% second
+timeUnit_1min=60/T_s;
+timeUnit_1hour=3600/T_s;
+timeUnit_1day=timeUnit_1hour*24;
+timeUnit_1week=7*timeUnit_1day;
+
+% TinIC = initial indoor temperature = 20 deg C
+TinIC = 0;
+TinHeatK=273+0; %[K] heating elements
+supp_water_initial_temp=0;         %   [Celsius]
+
+
+
+                            % -------------------------------
+                            % Problem constant
+                            % -------------------------------
+                            % converst radians to degrees
+                            r2d = 180/pi;
+                            % -------------------------------
+                            % Define the house geometry
+                            % -------------------------------
+                            % House length = 30 m
+                            lenHouse = 30;
+                            % House width = 10 m
+                            widHouse = 10;
+                            % House height = 4 m
+                            htHouse = 4;
+                            % Roof pitch = 40 deg
+                            pitRoof = 40/r2d;
+                            % Number of windows = 6
+                            numWindows = 6;
+                            % Height of windows = 1 m
+                            htWindows = 1;
+                            % Width of windows = 1 m
+                            widWindows = 1;
+                            windowArea = numWindows*htWindows*widWindows;
+                            wallArea = 2*lenHouse*htHouse + 2*widHouse*htHouse + ...
+                                       2*(1/cos(pitRoof/2))*widHouse*lenHouse + ...
+                                       tan(pitRoof)*widHouse - windowArea;
+                            % -------------------------------
+                            % Define the type of insulation used
+                            % -------------------------------
+                            % Glass wool in the walls, 0.2 m thick
+                            % k is in units of J/sec/m/C                                                       NOT convert to J/hr/m/C multiplying by 3600
+                            kWall = 0.038;   % second is the time unit
+                            LWall = .2;
+                            RWall = LWall/(kWall*wallArea);
+                            % Glass windows, 0.01 m thick
+                            kWindow = 0.78;  % second is the time unit
+                            LWindow = .01;
+                            RWindow = LWindow/(kWindow*windowArea);
+                            % -------------------------------
+                            % Determine the equivalent thermal resistance for the whole building
+                            % -------------------------------
+                            Req = RWall*RWindow/(RWall + RWindow);
 % c = cp of air (273 K) = 1005.4 J/kg-K
 c = 1005.4;
-% -------------------------------
-% Enter the temperature of the heated air
-% -------------------------------
-% The air exiting the heater has a constant temperature which is a heater
-% property. THeater = 50 deg C
-THeater = 50;
-                                                                                % Air flow rate Mdot = 1 kg/sec = 3600 kg/hr
+                            % -------------------------------
+                            % Enter the temperature of the heated air
+                            % -------------------------------
+                            % The air exiting the heater has a constant temperature which is a heater
+                            % property. THeater = 50 deg C
+                            THeater = 50;
+                                                                                                            % Air flow rate Mdot = 1 kg/sec = 3600 kg/hr
                                                                                 Mdot = 3600;  % hour is the time unit
 % energy per hour is c*Mdot*?t
 % -------------------------------
@@ -124,7 +138,4 @@ M_air = 51.5*densAir;
 % 1 kW-hr = 3.6e6 J
 % cost = $0.09 per 3.6e6 J
 cost = 0.09/3.6e6;
-% TinIC = initial indoor temperature = 20 deg C
-TinIC = 0;
-TinHeatK=273+0; %[K] heating elements
-supp_water_initial_temp=0;         %   [Celsius]
+
