3e426e4ba8b9f5f63de4bfa36b390259d2ef2e38
Gyulai László <gyulailacko@gmail.com>
2019-05-16 19:28:11 +0200

    signal preview

--- model/simScale.m
+++ model/simScale.m
@@ -1,7 +1,7 @@
 
 %load('slab_and_radiator_and_mpc.mat')
 
-C=mpc(tf_19_toMPC,1800,20,1);
+C=mpc(tf_19_toMPC,1800,48,1);
 C.MV = struct('Min',{0;0},'Max',1,'RateMin',-1,'RateMax',1);
 C.OV = struct('Min',0,'Max',320,'ScaleFactor',1);
 C.D.ScaleFactor=1;
@@ -9,31 +9,33 @@ C.D.ScaleFactor=1;
 %% C1
 C1=C;
 C1.Weights.OutputVariables = {0.1};
-C1.Weights.ManipulatedVariables = {[0.1,0.01;0.01,0.005]};
+C1.Weights.ManipulatedVariables = {[0.1,0.2;0.01,0.1]};
 C1.Weights.ManipulatedVariablesRate = {[4,2;1.5,4]};
 
 %% C2 -- csak radiatorozik
 C2=C;
-C2.Weights.OutputVariables = {0.1};
-C2.Weights.ManipulatedVariables = {[10,0;1,0.15]};
+C2.PredictionHorizon=36;
+C2.Weights.OutputVariables = {0.3};
+C2.Weights.ManipulatedVariables = {[0.2,0;0,0.1]};
 C2.Weights.ManipulatedVariablesRate = {[20,0;0,5]};
 
 %% C3 -- csak radiatorozik es fos
 C3=C;
-C3.Weights.OutputVariables = {0.01};
-C3.Weights.ManipulatedVariables = {[10,0;1,0.15]};
-C3.Weights.ManipulatedVariablesRate = {[20,0;0,5]};
+C3.Weights.OutputVariables = {0.1};
+C3.Weights.ManipulatedVariables = {[0.2,0.005;0.005,0.005]};
+C3.Weights.ManipulatedVariablesRate = {[10,2.5;2.5,1]};
 
 %% C4 -- 0.01-es hiba, alacsony energia, mindketto beavatkozo
 C4=C;
-C4.Weights.OutputVariables = {0.1315}; % akar felmehet 0.145-ig is
+C4.PredictionHorizon=48;
+C4.Weights.OutputVariables = {2}; % akar felmehet 0.145-ig is
 C4.Weights.ManipulatedVariables = {[1,0.1;0.1,0.7]};
 C4.Weights.ManipulatedVariablesRate = {[20,0;0,5]};
 
 
 %% C5 -- nagy komfort (referencia kovetes) sok radiatorral
 C5=C;
-C5.Weights.OutputVariables = {0.75}; % akar felmehet 0.145-ig is
+C5.Weights.OutputVariables = {0.5}; % akar felmehet 0.145-ig is
 C5.Weights.ManipulatedVariables = {[1,0.5;0.125,0.2]};
 C5.Weights.ManipulatedVariablesRate = {[10,5;3,10]};
 
