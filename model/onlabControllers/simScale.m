118c8f5d0ae5545c475e69accc72327bd9252c3d
gyulas <gyulailacko@gmail.com>
2019-05-18 20:39:43 +0200

    More plotting functions

--- model/onlabControllers/simScale.m
+++ model/onlabControllers/simScale.m
@@ -5,6 +5,14 @@ C=mpc(tf_19_toMPC,1800,48,1);
 C.MV = struct('Min',{0;0},'Max',1,'RateMin',-1,'RateMax',1);
 C.OV = struct('Min',0,'Max',320,'ScaleFactor',1);
 C.D.ScaleFactor=1;
+%% const function analysis
+for i=1:10
+    for j=1:10
+        Z(i,j)=[i/10 j/10]*diag(C.Weights.ManipulatedVariables)*[i/10;j/10];
+    end
+end
+surf(1:10,1:10,Z)
+hold on
 
 %% C1
 C1=C;
@@ -17,6 +25,16 @@ C2=C;
 C2.Weights.OutputVariables = {0.3};
 C2.Weights.ManipulatedVariables = {[0.2,0;0,0.1]};
 C2.Weights.ManipulatedVariablesRate = {[20,0;0,5]};
+%% const function analysis
+for i=1:10
+    for j=1:10
+        Z2(i,j)=[i/10 j/10]*C2.Weights.ManipulatedVariables{:,:}*[i/10;j/10];
+    end
+end
+surf(1:10,1:10,Z2)
+hold on
+C2.Weights.ManipulatedVariables{:,:}
+
 
 %% C3 -- csak radiatorozik es fos
 C3=C;
@@ -29,6 +47,17 @@ C4=C;
 C4.Weights.OutputVariables = {1.5}; % akar felmehet 0.145-ig is
 C4.Weights.ManipulatedVariables = {[1,0.3;0.3,0.7]};
 C4.Weights.ManipulatedVariablesRate = {[500,0;0,100]};
+
+%% cost function -- MV
+
+for i=1:10
+    for j=1:10
+        Z4(i,j)=[i/10 j/10]*C4.Weights.ManipulatedVariables{:,:}*[i/10;j/10];
+    end
+end
+surf(1:10,1:10,Z4)
+C4.Weights.ManipulatedVariables{:,:}
+
 
 %% C5 -- nagy komfort (referencia kovetes) sok radiatorral
 C5=C;
@@ -85,9 +114,27 @@ C2Last10=processHeatData(C2Last10);
 CLast10=processHeatData(CLast10);
 
 %%
+EnergyConsumption.radiator=[CLast10.signals(4).values(4,6) ...
+                        C1Last10.signals(4).values(4,6) ...
+                        C2Last10.signals(4).values(4,6) ...
+                        C4Last10.signals(4).values(4,6) ...
+                        C5Last10.signals(4).values(4,6)];
+EnergyConsumption.padlofutes=[CLast10.signals(4).values(4,7) ...
+                        C1Last10.signals(4).values(4,7) ...
+                        C2Last10.signals(4).values(4,7) ...
+                        C4Last10.signals(4).values(4,7) ...
+                        C5Last10.signals(4).values(4,7)];
+EnergyConsumption.both=[EnergyConsumption.radiator' EnergyConsumption.padlofutes'];
+figure
+bar([1 2 3 4 5], EnergyConsumption.both, 'stacked')
+% hold on
+% bar([1 2 3 4 5], EnergyConsumption.padlofutes)
+
+
+%%
 % Komfort violation:
 f1=figure;
-title('Költségek és komfort reference preview mellett')    
+title('KÃ¶ltsÃ©gek Ã©s komfort reference preview mellett')    
 
 set(f1, 'OuterPosition', [672 441 576 622]);
 
@@ -101,9 +148,9 @@ ComfortViolation.max=[CLast10.signals(4).values(4,9) ...
                         C5Last10.signals(4).values(4,9)];                        
 bar([1 2 3 4 5], ComfortViolation.max)
 grid on
-title('Hibaintegrál')    
+title('HibaintegrÃ¡l')    
 ylab=get(gca,'YLabel');
-ylab.String={'hiba abszolút összege';'[Celsius fok * óra]'};
+ylab.String={'hiba abszolÃºt Ã¶sszege';'[Celsius fok * Ã³ra]'};
 set(gca,'YLabel',ylab)
 
 
@@ -130,7 +177,7 @@ bar([1 2 3 4 5], RequiredPower.max)
 hold on
 grid on
 bar([1 2 3 4 5], EnergyConsumption.sum)
-title('Költségek')
+title('KÃ¶ltsÃ©gek')
 
 
 legend('Peak Demand [W]','EnergyConsumption [kJ]')
@@ -145,7 +192,7 @@ set(gca,'YLim',lim);
 
 set(gca,'XTickLabel',{'CLast10','C1Last10','C2Last10','C4Last10','C5Last10'})
 xlab=get(gca,'XLabel');
-xlab.String='idõ (nap)';
+xlab.String='idÅ‘ (nap)';
 set(gca,'XLabel',xlab)
 
 shg
